1649538bb480ff46f3a02e49cd38e182eaaa0258
Mauricio Mantilla <mauricio.mantilla.ruales@gmail.com>
2023-05-13 12:23:54 -0500

    Crear funciones para realizar los pasos de suma

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 //
 //  main.cpp
-//  Proyecto Final J
+//  PROYECTO TOP
 //
-//  Created by Theo Almeida on 4/5/23.
+//  Created by Theo Almeida on 11/5/23.
 //
+
 
 #include <iostream>
 #include <vector>
@@ -11,143 +12,212 @@
 #include <sstream>
 using namespace std;
 
-struct tPolinomio {
-    long double coef;      /* Coeficientes */
-    long double grad;                /* Grado del polinomio */
-
+struct tPolinomio{
+    vector<double> coef;      /* Coeficientes */
+    int grado;                /* Grado del polinomio */
 };
 
+void funcion_monom(string frase, vector<string> &polinomios);
+void funcion_apart(vector<string> &polinomios,vector<string> &coeficientes,vector<string> &grados);
+void funcion_stringtonum(vector<string> &coeficientes,vector<string> &grados,vector< double> &coeficientes_double,vector<int> &grados_int);
+int funcion_maxgrado(vector<int> &grados_int);
+void funcion_order_set(vector< double> &coeficientes_double,vector<int> &grados_int,int maximum,int contador, tPolinomio &Pol_num);
+double funcion_Maxdegree(vector<tPolinomio> &Zpolinomio);
+void funcion_definep(vector<tPolinomio> &DEFPolinom,vector<tPolinomio> &Zpolinomio,double tmax);
+void funcion_add(vector<tPolinomio> &DEFPolinom, vector<double> &result,double tmax);
+void funcion_print(vector<double> &result,double tmax);
+
+
 int main(int argc, const char * argv[]) {
-    int nada,cantidad=0;
-    vector<vector<tPolinomio>> POLIM;
+    int cantidad=0;
+    vector<tPolinomio> Zpolinomio;
+        //PRIMER WHILE O FUNCION QUE VALIDE QUE EL NUMERO DE POLINOMIOS A SUMAR SEA UN NUMERO
     cout<<"Introduzca la cantidad de polinomios a sumar: ";
     cin>>cantidad;
     cin.ignore();
+        //HASTA AQUI
     for(int f=0;f<cantidad;f++){
-        //.............................Inicio de la lectura de polinomios.................................//
-    //-----------------LECTURA DEL STRING FRASE SEPARANDO EN TERMINOS (ARREGLAR EL SIGNO NEGATIVO DE FRASE[0]) ---------------------//
-        string frase,cadena="";
-        vector <string> polinomios;
+        //SEGUNDO WHILE O FUNCION QUE VALIDE QUE EL POLINOMIO INTRODUCIDO ESTE BIEN DIGITADO ( 5 3*x2 ≠ 53*x2)( 5 espacio 3*x2 == ERROR)
+        string frase;
         cout<<" Ingrese la cadena del polinomio "<<f+1<<": ";
-        getline(cin,frase);                 //Obtenemos una frase de texto leida caracter por caracter
-        for(int i=0;i<=frase.size();i++){
-            //Eliminamos los espacios
-            if(frase[i]==' ')
-                nada=0;
-            //Separamos los polinomios por -
-            else if( frase[i]=='-' and frase[0]!='-'){
-                polinomios.push_back(cadena);
-                cadena="-";}
-            //Separamos los polinomios por +
-            else if(frase[i]=='+' and frase[0]!='+'){
-                polinomios.push_back(cadena);
-                cadena="";}
-            //Unimos los caracteres acotados entre + y -
-            else if(frase[i]!= '+' and frase[i]!= '-' and i!=frase.size())
-                cadena+=frase[i];
-            else if(i==frase.size())
-                polinomios.push_back(cadena);
-            else if(frase[0]=='-')
-                cadena="-";
-            else if(frase[0]=='+')
-                cadena="";
-        }
-        //-----------------------------SEPARAR EL ELEMENTO DEL VECTOR EN SUS DOS COMPONENTES-----------------------------------//
+        getline(cin,frase);
+        //HASTA AQUI
+    //..................................Inicio de la lectura de polinomios....................................//
+        
+        // PASO 1:  Separamos la frase en monomios dentro de un vector
+        vector <string> polinomios;
+        funcion_monom(frase, polinomios);
+        
+        //PASO 2:   Separamos cada monomio en su coeficiente y grado
         vector<string> coeficientes;
         vector<string> grados;
-        for(int i=0;i<polinomios.size();i++)   {                //Leemos cada elemento del vector polinomios
-            cadena="";
-            //Lectura de la constante//
-            for(int j=0; j<=polinomios[i].size();j++){
-                cadena+=polinomios[i][j];
-                if(j==polinomios[i].size()){
-                    coeficientes.insert(coeficientes.begin(), cadena);
-                    cadena="0";
-                    grados.insert(grados.begin(),cadena);
-                    cadena="";
-                    nada=10;
-                }
-                else if(polinomios[i][j]== 'x'){
-                    cadena="";
-                    break;}}
-            //Fin de la constante//
-            for(int j=0; j<=polinomios[i].size();j++){           //Leemos cada caracter del elemento
-                if(polinomios[i][j]!= '*' and polinomios[i][j]!= 'x'){  //Obtenemos los coeficientes de cada expresion
-                    cadena+=polinomios[i][j];
-                    if(j==polinomios[i].size() and nada!=10)
-                        grados.push_back(cadena);
-                }
-                else if(polinomios[i][j]== 'x'){
-                    coeficientes.push_back(cadena);
-                    cadena="";}}
-            nada=0;
-        }
-        //-----------------------------TRANSFORMACION DE LOS ELEMENTOS DE STRINGS A FLOATS-----------------------------------//
-        vector<long double> coeficientes_ldouble;
-        vector<long double> grados_ldouble;
-        for(int i=0;i<coeficientes.size();i++){
-            coeficientes_ldouble.push_back(stold(coeficientes[i]));
-            grados_ldouble.push_back(stold(grados[i]));
-        }
-        //-----------------------------ORDENAMIENTO Y ASIGNACION A UN VECTOR FIJO -----------------------------------//
-        long double maximum=-999999999999999;
-        for(int i=0;i<coeficientes.size();i++){
-            if(grados_ldouble[i]>maximum)
-                maximum=grados_ldouble[i];}
-        vector<long double> polinom_c(maximum+1,0);           //Creacion de vectores del tamaño de su coeficiente máximo
-        vector<long double> polinom_g(maximum+1,0);
-        for(int i=0;i<coeficientes_ldouble.size();i++){
-            long double k=grados_ldouble[i];
-            polinom_g[k]=grados_ldouble[i];
-            polinom_c[k]=coeficientes_ldouble[i];
-        }
-        //-----------------------------ASIGNACION AL VECTOR MADRE -----------------------------------//
-        //Asignacion a una estructura//
-        vector<tPolinomio> Zpolinomio;
-        for(int i=0;i<polinom_c.size();i++){
-            tPolinomio temporal;
-            temporal.grad= {polinom_g[i]};
-            temporal.coef= {polinom_c[i]};
-            Zpolinomio.push_back(temporal);
-            temporal={0,0};}
-        //Asignacion de la estructura al vector madre//
-        POLIM.push_back(Zpolinomio);
-        for(int u=0;u<polinom_c.size();u++){
-            cout<<"Vector "<<u<<" es: "<<polinom_c[u]<<"*x"<<polinom_g[u]<<endl;
-        }
+        funcion_apart(polinomios, coeficientes, grados);
         
+        //PASO 3:   Convertimos cada coeficiente y grado expresado en strings a numeros
+        vector< double> coeficientes_double;
+        vector<int> grados_int;
+        funcion_stringtonum(coeficientes, grados, coeficientes_double, grados_int);
+        
+        //PASO 4:   Hallamos el grado maximo de solo los monomios del polinomio ingresado
+        int maximum=funcion_maxgrado(grados_int);
+        
+        //PASO 5:   Se ordena los coeficientes y se los asigna a una estructura.
+        tPolinomio Pol_num;
+        int contador=f;
+        funcion_order_set(coeficientes_double, grados_int, maximum, contador, Pol_num);
+        
+        //PASO 6:   Asignamos el polinomio a un vector con más polinomios
+        Zpolinomio.push_back(Pol_num);
+        
+        //PASO 7:   Vaciar todos los vectores que se han inicializado y creado a excepcion de Zpolinomio como vector madre
         polinomios.clear();
         coeficientes.clear();
         grados.clear();
-        coeficientes_ldouble.clear();
-        grados_ldouble.clear();
-        polinom_c.clear();
-        polinom_g.clear();
-        Zpolinomio.clear();
+        coeficientes_double.clear();
+        grados_int.clear();
+        Pol_num.coef.clear();
         
-        for(int u=0;u<polinom_c.size();u++){
-            cout<<"Vector "<<u<<" es: "<<polinom_c[u]<<"*x"<<polinom_g[u]<<endl;
-        }
     }
-    //.............................Fin de la lectura de un solo polinomio.................................//
- 
+//.......................................Fin de la lectura de un solo polinomio...................................................//
+        //PASO 8:    Hallamos el grado maximo entre todos los polinomios ingresados
+        double tmax=funcion_Maxdegree(Zpolinomio);
+        //PASO 9:   Definimos a todos los polinomios con un tamaño igual al maximo
+        vector<tPolinomio> DEFPolinom;
+        funcion_definep(DEFPolinom, Zpolinomio, tmax);
+        //PASO 10:  Se Suma todos los componentes del polinomio
+        vector<double> result;
+        funcion_add(DEFPolinom, result, tmax);
+        //PASO 11: Se imprime la suma de los polinomios
+        funcion_print(result, tmax);
+        cout<<endl;
     return 0;
-  }
+}
+                                    //EJEMPLOS//
+//                                                     grados ----> tamaño
+//              -3*x5 + 5.12*x4 -32*x3+8*x7+44*x1-9               ---> 0 2 3 4 5 7 ----> 6
+//               43*x5 + 50*x4 -12*x3+22*x7+22*x2+19             ---> 0 2 3 4 5 7 ----> 6
+//               57*x5 + 3*x9 +3*x13+3*x7+3*x17+17               ---> 0 5 7 9 13 17 ----> 6
+//               3*x5 -5.12*x4 +32*x3-8*x7-42*x1-9               ---> 0 2 3 4 5 7 ----> 6
 
-
-
-
-//-3*x5 + 5.12*x4 -32*x3+8*x7+4*x2-9
-// 43*x5 + 50*x4 -12*x3+22*x7+12*x2+11
-// 57*x5 + 3*x9 +3*x13+3*x7+3*x17+13
-
-
-//          REVISAR TAM DE TODOS LOS VECTORES
-//cout<<"polinomios en string:    "<<polinomios.size()<<endl;
-//cout<<"coeficientes en string:      "<<coeficientes.size()<<endl;
-//cout<<"grados en string:        "<<grados.size()<<endl;
-//cout<<"coeficientes en double:      "<<coeficientes_ldouble.size()<<endl;
-//cout<<"grados en double:     "<<grados_ldouble.size()<<endl;
-//cout<<"tamaño coeficiente final:        "<<polinom_c.size()<<endl;
-//cout<<"tamaño grados final:     "<<polinom_g.size()<<endl;
+void funcion_monom(string frase, vector<string> &polinomios){
+    int nada=0;
+    string cadena="";
+    for(int i=0;i<=frase.size();i++){
+        //Eliminamos los espacios
+        if(frase[i]==' ')
+            nada=0;
+        //Inicializar el signo//
+        else if(frase[i]=='-' and i==0)
+            cadena="-";
+        else if(frase[i]=='+' and  i==0)
+            cadena="";
+        //Separamos los polinomios por -
+        else if( frase[i]=='-' and i!=0){
+            polinomios.push_back(cadena);
+            cadena="-";}
+        //Separamos los polinomios por +
+        else if(frase[i]=='+' and  i!=0){
+            polinomios.push_back(cadena);
+            cadena="";}
+        //Unimos los caracteres acotados entre + y -
+        else if(frase[i]!= '+' and frase[i]!= '-' and i!=frase.size())
+            cadena+=frase[i];
+        if(i==frase.size())
+            polinomios.push_back(cadena);}
+};
+void funcion_apart(vector<string> &polinomios,vector<string> &coeficientes,vector<string> &grados){
+    int nada=0;
+    for(int i=0;i<polinomios.size();i++)   {                //Leemos cada elemento del vector polinomios
+       string cadena="";
+                //Lectura de la constante//
+        for(int j=0; j<=polinomios[i].size();j++){
+            cadena+=polinomios[i][j];
+            if(j==polinomios[i].size()){
+                coeficientes.insert(coeficientes.begin(), cadena);
+                cadena="0";
+                grados.insert(grados.begin(),cadena);
+                cadena="";
+                nada=10;
+            }
+            else if(polinomios[i][j]== 'x'){
+                cadena="";
+                break;}}
+                //Fin de la constante//
+ 
+        
+        for(int j=0; j<=polinomios[i].size();j++){           //Leemos cada caracter del elemento
+            if(polinomios[i][j]!= '*' and polinomios[i][j]!= 'x'){  //Obtenemos los coeficientes de cada expresion
+                cadena+=polinomios[i][j];
+                 if(j==polinomios[i].size() and nada!=10)
+                    grados.push_back(cadena);
+            }
+            else if(polinomios[i][j]== 'x'){
+                coeficientes.push_back(cadena);
+                cadena="";}}
+        nada=0;}
+};
+void funcion_stringtonum(vector<string> &coeficientes,vector<string> &grados,vector< double> &coeficientes_double,vector<int> &grados_int){
+    for(int i=0;i<coeficientes.size();i++){
+        coeficientes_double.push_back(stod(coeficientes[i]));
+        grados_int.push_back(stoi(grados[i]));}
+};
+int funcion_maxgrado(vector<int> &grados_int){
+    int maximum=-1;
+    for(int i=0;i<grados_int.size();i++){
+        if(grados_int[i]>maximum)
+            maximum=grados_int[i];}
+    return maximum;
+}
+void funcion_order_set(vector< double> &coeficientes_double,vector<int> &grados_int,int maximum,int contador, tPolinomio &Pol_num){
+    vector<double>coef_aux(maximum+1,0);
+    for(int i=0;i<grados_int.size();i++){
+        int k=grados_int[i];
+        coef_aux[k]=coeficientes_double[i];
+    }
+    Pol_num.coef=coef_aux;
+    Pol_num.grado=contador;
+};
+double funcion_Maxdegree(vector<tPolinomio> &Zpolinomio){
+    double tmax=0;
+    for(int i=0;i<Zpolinomio.size();i++){
+        if(tmax<Zpolinomio[i].coef.size())
+            tmax=Zpolinomio[i].coef.size();}
+    return tmax;
+};
+void funcion_definep(vector<tPolinomio> &DEFPolinom,vector<tPolinomio> &Zpolinomio,double tmax){
+    
+    for(int f=0;f<Zpolinomio.size();f++){
+        vector<double>coef2_aux(tmax,0);
+        for(int i=0;i<Zpolinomio[f].coef.size();i++){
+            coef2_aux[i]=Zpolinomio[f].coef[i];
+        }
+        tPolinomio Pol_aux;
+        Pol_aux.coef=coef2_aux;
+        Pol_aux.grado=f;
+        DEFPolinom.push_back(Pol_aux);
+    }
+};
+void funcion_add(vector<tPolinomio> &DEFPolinom, vector<double> &result,double tmax){
+    for(int j=0;j<tmax;j++){
+        double sumas=0;
+        for(int i=0;i<DEFPolinom.size();i++){
+            sumas+=DEFPolinom[i].coef[j];
+        }
+        result.push_back(sumas);
+        sumas=0;
+    }
+};
+void funcion_print(vector<double> &result,double tmax){
+    int nada=0;
+    cout<<endl<<"El polinomio suma es de grado "<<tmax-1<<" :"<<endl;
+    for(int i=0;i<result.size();i++){
+        if(result[i]!=0 and i==0 )
+            cout<<result[i]<<" ";
+        else if(result[i]!=0 and result[i]>0 )
+            cout<<"+ "<<result[i]<<"*x"<<i<<" ";
+        else if(result[i]!=0 and result[i]<0)
+            cout<<result[i]<<"*x"<<i<<" ";
+        else if(result[i]==0)
+            nada=3.1416;}
+};
 
